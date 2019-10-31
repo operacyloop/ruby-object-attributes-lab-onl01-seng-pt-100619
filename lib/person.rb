488b4cc4d1a3f44a2b65589e3f4b1837e02abdd3
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-31 19:19:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,22 @@
 class Person
+  
+ #  def initialize(first_name, last_name)
+#   @first_name = first_name
+ #   @last_name = last_name
+#  end
+ 
+ # GETTER METHOD:
+  def name=(name)
+    @name = name
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  # SETTER METHOD:
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end
+ 
+end
+
 end 
