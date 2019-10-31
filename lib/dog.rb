c3a3ae5d1a0e247554cb7f6d862c8352115af36f
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-31 19:24:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,4 +9,15 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
+  
+  # GETTER METHOD:
+  def breed
+    @breed
+  end
+  
+  # SETTER METHOD:
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
 end
