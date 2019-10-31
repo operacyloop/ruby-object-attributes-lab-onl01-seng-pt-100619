091666d4e0bd2b002a8c23adedc019950da833d3
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-31 19:25:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog 
+  
+  # GETTER METHOD:
+  def name
+    @name
+  end
+  
+  # SETTER METHOD:
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  # GETTER METHOD:
+  def breed
+    @breed
+  end
+  
+  # SETTER METHOD:
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+  
+# def initialize(name)
+#    @name = name
+#  end
+ 
+ # GETTER METHOD:
+  def name
+    @name
+  end
+  
+  # SETTER METHOD:
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  # GETTER METHOD:
+  def job
+    @job
+  end
+  
+  # SETTER METHOD:
+  def job=(new_job)
+    @job = new_job
+  end
+ 
+end 
